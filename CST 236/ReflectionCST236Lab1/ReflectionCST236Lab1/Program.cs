844e08cd2adefa4afa73a3aa8093baec6373bc17
masterofsync <bikzzzzz@gmail.com>
2017-04-24 12:52:25 -0700

    Lab 1 CST 236 (Software Testing) at OIT
    Use of C# Reflection to get various properties of methods.

---
+++ CST 236/ReflectionCST236Lab1/ReflectionCST236Lab1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TestClassLibrary;
+
+namespace ReflectionCST236Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Class1 test = new Class1();
+           Type t = test.GetType();
+           Console.WriteLine("Type Name: " + t.Name + Environment.NewLine);
+           Console.WriteLine("Namespace: " + t.Namespace + Environment.NewLine);
+           Console.WriteLine("BaseType: " + t.BaseType + Environment.NewLine);
+
+
+        //private data members
+           Console.WriteLine(Environment.NewLine + "Private data Members:");
+            FieldInfo[] fields = t.GetFields(
+                         BindingFlags.NonPublic | 
+                         BindingFlags.Instance);
+
+            foreach (var privateMembers in fields)
+            {
+                Console.WriteLine(privateMembers.ToString());
+            }
+
+        
+            //get public methods
+            Console.WriteLine(Environment.NewLine+"Public Method Signatures:");
+            MethodInfo[] myArrayMethodInfo = t.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+            foreach (var publicMethod in myArrayMethodInfo)
+            {
+               Console.WriteLine(publicMethod.ToString());
+            }
+        }
+    }
+}
---
+++ CST 236/ReflectionCST236Lab1/TestClassLibrary/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassLibrary
+{
+    public class Class1
+    {
+        private string myString = "abce";
+        private int myInteger = 54;
+
+        private int GetSum(int x, int y)
+        {
+            return x + y;
+        }
+
+        private int GetSubtraction(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Sum(int x, int y)
+        {
+            return GetSum(x,y);
+        }
+
+        public int Subtraction(int x, int y)
+        {
+            return GetSubtraction(x, y);
+        }
+
+        public int Multiply(int x, int y)
+        {
+            return x*y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x/y;
+        }
+
+
+    }
+}
